e0ee14a5ff2c7fb1332675350855c106e452be57
Lan Internet <mh@laninternet.com>
2025-12-14 08:59:05 +0000

    Sorry for the wait folks!
    Many new features!
    - Copilot Nuker 
    - Password Expiry Disabler
    - Built in MAS activator
    - Fix of many existing features

--- BatchUtil/regchg.bat
+++ BatchUtil/regchg.bat
@@ -1,40 +1,123 @@
 @echo off
+set RESTARTCOMPUTER=0
+setlocal enabledelayedexpansion
 net session >nul 2>&1
 if %errorLevel% neq 0 (
-    echo Please wait for admin privileges to be authorized
+    echo Please wait for admin privileges to be authorised. Admin privileges must be present in order for regchg to run properly.
     powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb RunAs"
     exit /b
 )
-
-echo *** REGISTRY EDITOR (regchg.bat, running w/Admin Permissions) - © Lan Internet Software***
+:START
+cls
+echo *** REGISTRY EDITOR IV - Revision B (regchg.bat, running w/Admin Permissions) - © Lan Internet Software ***
+echo.
+echo.
 echo This program will make it easy for you to disable certain annoying Windows Features.
-echo WARNING! The uninstall Edge feature is completely functional but may throw errors due to the way the program deletes it. This program deletes Edge by forcibly removing all Edge-related functions, even if they don't exist.
-echo For users that use applications that rely on Microsoft Edge WebView2, it is highly recommended that you do not uninstall Microsoft Edge as applications that rely on WebView2 also rely on core functions of Microsoft Edge.
+echo WARNING! The uninstall Edge feature is completely functional but may throw errors due to the way the program deletes it. This program deletes Edge by forcibly removing all Edge-related functions, even if they don't exist
+echo.
+echo IMPORTANT For users that use applications that rely on Microsoft Edge WebView2, it is highly recommended that you do not uninstall Microsoft Edge as applications that rely on WebView2 also rely on core functions of Microsoft Edge.
+echo.
+echo. NOTE: It is required to restart your computer so that the changes are applied properly after using any option of this script
+echo.
+echo In the Everything options, all users will have their password expiry disabled.
 echo [1] Disable Start Menu Search Results - Will make it easier to find what you want
 echo [2] Enable Verbose Boot Messages - This will make the messages upon startup, login and shutdown. Useful for slow PC
 echo [3] Uninstall Edge
 echo [4] Restore Windows 10 style Right-click menu - Useful for long-time Windows users.
-echo [5] Everything
-echo [6] Quit to Windows
-choice /c:123456 /m "Choose an option : "
-IF ERRORLEVEL 6 GOTO END
+echo [5] Everything 
+echo [6] Everything except Edge Uninstall 
+echo [7] Disable Microsoft Copilot
+echo [8] Disable password expiry for all users
+echo [9] Disable password expiry for a user
+echo [0] Quit Program
+echo [M] MAS Windows Activator (requires Internet Connection!)
+choice /c:1234567890M /m "Choose an option : "
+IF ERRORLEVEL 11 GOTO MAS
+IF ERRORLEVEL 10 GOTO END
+IF ERRORLEVEL 9 GOTO SINGLEUSER
+IF ERRORLEVEL 8 GOTO ALLUSERS
+IF ERRORLEVEL 7 GOTO WINDOWSAI
+IF ERRORLEVEL 6 GOTO EVERYTHING
 IF ERRORLEVEL 5 GOTO AIO
 IF ERRORLEVEL 4 GOTO W10
 IF ERRORLEVEL 3 GOTO EDGE
 IF ERRORLEVEL 2 GOTO VBM
 IF ERRORLEVEL 1 GOTO WINSEARCH
-:END
-exit
-:W10
-reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
+:MAS
+echo If this program appears frozen after you've exited MAS, hold the CTRL key, then press C (CTRL-C). If it asks "Terminate batch job?", use N.
+powershell /c "irm https://get.activated.win | iex"
+echo If there is no red text or error messages, this means that the operation was successful!
 pause
 goto END
+
+:ALLUSERS
+powershell /c "Get-LocalUser | Set-LocalUser -PasswordNeverExpires $true"
+echo If there is no red text or error messages, this means that the operation was successful!
+pause
+goto END
+
+:SINGLEUSER
+set /p username="Enter the name of the user: "
+powershell /c Set-LocalUser -Name '%username%' -PasswordNeverExpires $true
+echo If there is no red text or error messages, this means that the operation was successful!
+pause
+goto END
+
+
+:END
+IF !RESTARTCOMPUTER!==1 GOTO ASKRESTART
+echo.
+echo Exit?
+choice
+IF ERRORLEVEL 2 GOTO START
+IF ERRORLEVEL 1 GOTO REALEND
+
+:REALEND
+exit /b
+
+:ASKRESTART
+echo.
+echo The changes have successfully been made. Your computer must be restarted in order to apply the changes properly. Restart?
+echo If you do NOT want to restart your computer, use N at this prompt and restart explorer manually. This script cannot do that due to the limitations of the batch programming language.
+choice
+set RESTARTCOMPUTER=0
+IF ERRORLEVEL 2 GOTO END
+IF ERRORLEVEL 1 GOTO REBOOT
+
+:REBOOT
+shutdown /r /t 10 /c "This computer will reboot in 10 seconds. Make sure to save all of your work. Changes will be applied during the reboot - REGCHG.BAT"
+
+:W10
+reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+set RESTARTCOMPUTER=1
+pause
+goto END
+
+:EVERYTHING
+reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+reg add HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Explorer /v DisableSearchBoxSuggestions /t REG_DWORD /d 1 /f
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search /v BingSearchedEnabled /t REG_DWORD /d 0 /f
+reg add HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\WindowsAI /v DisableAIDataAnalysis /d 1 /f
+reg add HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\WindowsCopilot /v TurnOffWindowsCopilot /d 1 /f
+powershell /c "Get-LocalUser | Set-LocalUser -PasswordNeverExpires $true"
+taskkill /f /im explorer.exe
+timeout /t 2 /nobreak
+set RESTARTCOMPUTER=1
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v VerboseStatus /t REG_DWORD /d 1 /f
+pause
+goto END
+
 :AIO
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 reg add HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Explorer /v DisableSearchBoxSuggestions /t REG_DWORD /d 1 /f
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search /v BingSearchedEnabled /t REG_DWORD /d 0 /f
+reg add HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\WindowsAI /v DisableAIDataAnalysis /d 1 /f
+reg add HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\WindowsCopilot /v TurnOffWindowsCopilot /d 1 /f
+powershell /c "Get-LocalUser | Set-LocalUser -PasswordNeverExpires $true"
 taskkill /f /im explorer.exe
 timeout /t 2 /nobreak
-explorer
+set RESTARTCOMPUTER=1
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v VerboseStatus /t REG_DWORD /d 1 /f
 taskkill /f /im msedge.exe
 taskkill /f /im MicrosoftEdgeUpdate.exe
@@ -53,6 +136,7 @@ del /f "C:\Users\Public\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch
 del /f "%USERPROFILE%\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Microsoft Edge.lnk"
 pause
 goto END
+
 :EDGE
 taskkill /f /im msedge.exe
 taskkill /f /im MicrosoftEdgeUpdate.exe
@@ -70,14 +154,27 @@ del /f "C:\Users\Public\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch
 del /f "%USERPROFILE%\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Microsoft Edge.lnk"
 pause
 goto END
+
 :VBM
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v VerboseStatus /t REG_DWORD /d 1 /f
 pause
 goto END
+
 :WINSEARCH
 reg add HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Explorer /v DisableSearchBoxSuggestions /t REG_DWORD /d 1 /f
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search /v BingSearchedEnabled /t REG_DWORD /d 0 /f
 taskkill /f /im explorer.exe
 timeout /t 2 /nobreak
-explorer
+set RESTARTCOMPUTER=1
 pause
 goto END
+
+:WINDOWSAI
+reg add HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\WindowsAI /v DisableAIDataAnalysis /d 1 /f
+reg add HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\WindowsCopilot /v TurnOffWindowsCopilot /d 1 /f
+pause
+goto END
+
+
+
+goto START
