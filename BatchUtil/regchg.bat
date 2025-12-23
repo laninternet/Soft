c61a250be9797edbd992f4ca857915cc95b8ce1f
Lan Internet <mh@laninternet.com>
2025-12-23 23:12:23 +0000

    Revision C added shell utils. Revision D adds explorer blank pop up fix
    again this release was approved first on the Software repository but here it is. Good day folks!

--- BatchUtil/regchg.bat
+++ BatchUtil/regchg.bat
@@ -9,7 +9,7 @@ if %errorLevel% neq 0 (
 )
 :START
 cls
-echo *** REGISTRY EDITOR IV - Revision B (regchg.bat, running w/Admin Permissions) - © Lan Internet Software ***
+echo *** REGISTRY EDITOR IV - Revision D (regchg.bat, running w/Admin Permissions) - © Lan Internet Software ***
 echo.
 echo.
 echo This program will make it easy for you to disable certain annoying Windows Features.
@@ -31,7 +31,11 @@ echo [8] Disable password expiry for all users
 echo [9] Disable password expiry for a user
 echo [0] Quit Program
 echo [M] MAS Windows Activator (requires Internet Connection!)
-choice /c:1234567890M /m "Choose an option : "
+echo [S] Configure Windows Shell
+echo [F] Fix Blank Explorer Warning Pop-up on startup
+choice /c:1234567890MSF /m "Choose an option : "
+IF ERRORLEVEL 13 GOTO FIX
+IF ERRORLEVEL 12 GOTO SHELL
 IF ERRORLEVEL 11 GOTO MAS
 IF ERRORLEVEL 10 GOTO END
 IF ERRORLEVEL 9 GOTO SINGLEUSER
@@ -43,6 +47,53 @@ IF ERRORLEVEL 4 GOTO W10
 IF ERRORLEVEL 3 GOTO EDGE
 IF ERRORLEVEL 2 GOTO VBM
 IF ERRORLEVEL 1 GOTO WINSEARCH
+
+:FIX
+echo.
+echo The way this function of this program works is that certain programs add a rogue registry key that causes Windows to think it has to load a startup file when it does not, however since the program in question didn't implement their files correctly, it causes Wwindows to display a blank error box on startup. This program will fix this annoying behaviour.
+echo.
+echo NOTE: The fix for this involves deleting a registry key. However since the name of the registry key has changed across Windows Versions, and REGCHG is designed to operate on the widest range of computers, you may see errors. For this function of this program, as long as at least one command completed successfully (or you see "The operation has completed successfully" at least once), this means that the fix has succeeded.
+echo If the system says "ERROR: The system was unable to find the specified registry key or value.", it can be safely ignored.
+echo.
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\Windows" /v Run /f 
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\Windows" /v Load /f 
+echo.
+pause
+goto END
+
+
+:SHELL
+cls
+echo *** REGISTRY EDITOR IV - Revision D (regchg.bat, running w/Admin Permissions) - © Lan Internet Software ***
+echo.
+echo The method in which this program functions is that when your Windows PC starts up, it checks a registry key to determine what will be the default shell (in this case EXPLORER.EXE). The shell is what you'll interact with after you've logged on. Some people replace the shell with more lightweight options (such as FreeCommander) or replace it with a completely different thing (such as a cash register/POS program. If you go to your local McDo, it's just regular Windows computers but with a custom shell)
+echo.
+echo [1] Set custom executable for Windows Shell
+echo [2] Reset shell to C:\WINDOWS\EXPLORER.EXE
+choice /c:12 /m "Choose an option: "
+IF ERRORLEVEL 2 GOTO RESETSHELL
+IF ERRORLEVEL 1 GOTO SHCONFIG
+
+:RESETSHELL
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "C:\Windows\EXPLORER.EXE" /f
+pause
+goto END
+
+:SHCONFIG
+echo.
+echo You may also specify command line arguments for your application when you will log on if your application supports it and if you want. Examples of valid applications and command line arguments:
+echo 1: C:\Windows\py.exe C:\\WinAIO\\G-AIO.py
+echo 2: C:\WINDOWS\SYSTEM32\CMD.EXE /C pause
+echo 3: D:\downloads\idunno.exe --help
+echo 4: C:\Windows\Notepad.exe
+echo.
+echo NOTE: You must specify the FULL FILE PATH to your application and make sure that the file is USABLE (not read/write protected). If you do not respect this rule, Windows will NOT be able to find your application and will display a black screen upon startup (you will have to start the Task Manager and start this program again, and reconfigure it which is doable but annoying so it is best to avoid these troubles now)
+echo.
+set /p shell=Enter the path to your executable name and any command line arguments: 
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "%shell%" /f
+pause
+goto END
+
 
 :MAS
 echo If this program appears frozen after you've exited MAS, hold the CTRL key, then press C (CTRL-C). If it asks "Terminate batch job?", use N.
